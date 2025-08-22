7efc7630cf1a3d57131a808f076e89960dc79bba
Michael <mkyrich33@gmail.com>
2025-08-22 15:03:13 -0700

    hh

--- Card.java
+++ Card.java
@@ -1,3 +1,25 @@
 public class Card {
-    
+    private int rank;
+    private int suit;
+
+    public Card (int r, int s) {
+        rank = r;
+        suit = s;
+    }
+
+    public int getRank () {
+        return rank;
+    }
+
+    public int getSuit () {
+        return suit;
+    }
+
+    public void setRank (int r) {
+        rank = r;
+    }
+
+    public void setSuit (int s) {
+        suit = s;
+    }
 }
--- CardMain.java
+++ CardMain.java
@@ -1,5 +1,14 @@
 public class CardMain {
     public static void main(String[] args) {
-        
+        Card c1 = new Card(7, 3);
+        Card c2 = new Card(2, 0);
+        Card c3 = new Card(11, 2);
+
+        System.out.println(c1.getSuit());
+        System.out.println(c2.getSuit());
+        System.out.println(c3.getRank());
+
+        c2.setSuit(1);
+        System.out.println("Edit: " + c2.getSuit());
     }
 }
