41ab4e27dabc16bdabf4ac7177aacb638a21ca25
Michael <mkyrich33@gmail.com>
2025-08-22 14:14:25 -0700

    hh

--- BodyOfWaterMain.java
+++ BodyOfWaterMain.java
@@ -1,6 +1,11 @@
 public class BodyOfWaterMain {
     public static void main(String[] args) {
-        BodyOfWater b = new BodyOfWater();
-        System.out.println("here");
+        BodyOfWater b = new BodyOfWater("Lake", 400, 300, false);
+        BodyOfWater b2 = new BodyOfWater("Pond", 85, 58.9, false);
+
+        b.printState();
+        b2.printState();
+        b2.SetName("Slough");
+        b2.printState();
     }
 }
